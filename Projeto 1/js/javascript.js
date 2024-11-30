70e83271898d2ffb97b75ec56122367bb030310d
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-11-30 13:23:34 -0300

    projeto 01  e 02

--- Projeto 1/js/javascript.js
+++ Projeto 1/js/javascript.js
@@ -8,9 +8,18 @@ hamburguer.addEventListener('click',function() {
     menu.children[0].style.display = 'flex';
     menu.children[0].style.flexDirection = 'column';
     boolean = true;
+    hamburguer.children[0].classList.remove('fa-solid');
+    hamburguer.children[0].classList.remove('fa-bars');
+    hamburguer.children[0].classList.add('fa-solid');
+    hamburguer.children[0].classList.add('fa-x');
     }else{
         menu.style.display = 'none';
         boolean = false;
+        hamburguer.children[0].classList.remove('fa-solid');
+        hamburguer.children[0].classList.remove('fa-x');
+        hamburguer.children[0].classList.add('fa-solid');
+        hamburguer.children[0].classList.add('fa-bars');
+        
     }
 console.log('teste');
 });
