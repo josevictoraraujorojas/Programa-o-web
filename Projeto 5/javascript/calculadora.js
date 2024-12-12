650a7b6b935dba912325b84e7d0bbb691a31fb02
josevictoraraujorojas <kaneki1212112121@gmail.com>
2024-12-12 16:31:49 -0300

    projeto05

--- Projeto 5/javascript/calculadora.js
+++ Projeto 5/javascript/calculadora.js
@@ -1,7 +1,7 @@
 // Seleciona o visor e todos os botões
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('#buttons .button');
-let posfixa = "";
+let posfixa = [];
 
 // Função para atualizar o visor
 function updateDisplay(value) {
@@ -12,7 +12,7 @@ function updateDisplay(value) {
 
 }
 function infixaParaPosfixa() {
-  posfixa = "";
+  posfixa = [];
   let pilha = [];
   const expression = display.value
     .replace(/,/g, '.')  // Substitui vírgula por ponto decimal
@@ -20,28 +20,36 @@ function infixaParaPosfixa() {
     .replace(/÷/g, '/'); // Substitui símbolo de divisão por /
 
   for (let i = 0; i < expression.length; i++) {
-    const char = expression.charAt(i);
+    console.log('teste:'+i);
+    let char = expression.charAt(i);
+    
 
-    if (!isNaN(char) && char !== " ") {
-      posfixa += char;
+    if (!isNaN(char) && char !== " " || char == ".") {
+      while( i < expression.length && !isNaN(expression.charAt(i+1)) && expression.charAt(i+1) !== " " || expression.charAt(i+1) == "." ){
+        char+=expression.charAt(i+1);
+        i+=1;
+      }
+      console.log(char);
+      posfixa.push(char);
     } else if (char === '(') {
       pilha.push(char);
     } else if (char === '+' || char === '-') {
+      console.log('teste');
       while (pilha.length > 0 && pilha[pilha.length - 1] !== '(') {
-        posfixa += pilha.pop();
+        posfixa.push(pilha.pop());
       }
       pilha.push(char);
     } else if (char === '*' || char === '/') {
 
       while ( pilha.length > 0 && (pilha[pilha.length - 1] === '*' || pilha[pilha.length - 1] === '/')) {
-        posfixa += pilha.pop();
+        posfixa.push(pilha.pop());
       }
       pilha.push(char);
     } else if (char === ')') {
 
       let retira = pilha.pop();
       while (retira !== '(') {
-        posfixa += retira;
+        posfixa.push(retira);
         retira = pilha.pop();
       }
     }
@@ -50,7 +58,7 @@ function infixaParaPosfixa() {
 
   while (pilha.length > 0) {
     const operador = pilha.pop();
-    posfixa += operador;
+    posfixa.push(operador);
   }
 }
 
@@ -58,24 +66,26 @@ function infixaParaPosfixa() {
 function calculate() {
 
   infixaParaPosfixa();
+
+  console.log(posfixa);
   let stack =[];
 
   for(i=0;i<posfixa.length;i++){
-    if(posfixa.charAt(i) == "+" || posfixa.charAt(i) == "-" || posfixa.charAt(i) == "*" || posfixa.charAt(i) == "/"){
-      let aux1 =parseInt( stack.pop());
-      let aux2 =parseInt( stack.pop());
+    if(posfixa[i] == "+" || posfixa[i] == "-" || posfixa[i] == "*" || posfixa[i] == "/"){
+      let aux1 =parseFloat( stack.pop());
+      let aux2 =parseFloat( stack.pop());
 
-      if(posfixa.charAt(i) == "+"){
-        stack.push(parseInt(aux1+aux2));
-      } else if(posfixa.charAt(i) == "-"){
-        stack.push(parseInt(aux1-aux2));
-      }else if(posfixa.charAt(i) == "*"){
-        stack.push(parseInt(aux1*aux2));
-      }else if(posfixa.charAt(i) == "/"){
-        stack.push(parseInt(aux1/aux2));
+      if(posfixa[i] == "+"){
+        stack.push(parseFloat(aux1+aux2));
+      } else if(posfixa[i] == "-"){
+        stack.push(parseFloat(aux1-aux2));
+      }else if(posfixa[i] == "*"){
+        stack.push(parseFloat(aux1*aux2));
+      }else if(posfixa[i] == "/"){
+        stack.push(parseFloat(aux1/aux2));
       }
     }else{
-      stack.push(posfixa.charAt(i))
+      stack.push(posfixa[i])
     }
   }
     
